f2da68362cd19b2ab05ce31358229008b9f98a00
StevenAlexanderJohnson <56038965+StevenAlexanderJohnson@users.noreply.github.com>
2026-08-18 22:33:28 -0400

    changing download to regular function

--- internal/factorio/download.go
+++ internal/factorio/download.go
@@ -10,7 +10,7 @@ import (
 	"time"
 )
 
-func DownloadFactorioServer(ctx context.Context, downloadUrl string, destPath string, fatalChan chan<- error) {
+func DownloadFactorioServer(ctx context.Context, downloadUrl string, destPath string) error {
 	client := &http.Client{
 		// Big timeout period just in case that it's hosted on a slow machine or slow internet
 		Timeout: 30 * time.Minute,
@@ -18,46 +18,40 @@ func DownloadFactorioServer(ctx context.Context, downloadUrl string, destPath st
 
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, downloadUrl, nil)
 	if err != nil {
-		fatalChan <- fmt.Errorf("failed to create request to download factorio: %w", err)
-		return
+		return fmt.Errorf("failed to create request to download factorio: %w", err)
 	}
 
 	resp, err := client.Do(req)
 	if err != nil {
-		fatalChan <- fmt.Errorf("http request to download factorio failed: %w", err)
-		return
+		return fmt.Errorf("http request to download factorio failed: %w", err)
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		fatalChan <- fmt.Errorf("unexpected status code while downloading factorio: %d", resp.StatusCode)
-		return
+		return fmt.Errorf("unexpected status code while downloading factorio: %d", resp.StatusCode)
 	}
 
 	out, err := os.Create(destPath + ".tmp")
 	if err != nil {
-		fatalChan <- fmt.Errorf("failed to create destination file for factorio: %w", err)
-		return
+		return fmt.Errorf("failed to create destination file for factorio: %w", err)
 	}
 	defer out.Close()
 
 	_, err = io.Copy(out, resp.Body)
 	if err != nil {
 		_ = os.Remove(destPath + ".tmp")
-		fatalChan <- fmt.Errorf("failed during downloading factorio: %w", err)
-		return
+		return fmt.Errorf("failed during downloading factorio: %w", err)
 	}
 
 	if err := os.Rename(destPath+".tmp", destPath); err != nil {
 		_ = os.Remove(destPath + ".tmp")
-		fatalChan <- fmt.Errorf("failed during renamed temp factorio file: %w", err)
-		return
+		return fmt.Errorf("failed during renamed temp factorio file: %w", err)
 	}
 
 	cmd := exec.CommandContext(ctx, "tar", "-xJf", destPath, "-C", destPath)
 	output, err := cmd.CombinedOutput()
 	if err != nil {
-		fatalChan <- fmt.Errorf("tar extraction failed: %w (output: %s)", err, string(output))
-		return
+		return fmt.Errorf("tar extraction failed: %w (output: %s)", err, string(output))
 	}
+	return nil
 }
